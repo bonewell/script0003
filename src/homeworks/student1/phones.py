eb757f417056430f0ea732f2f154f264e76f6c4f
Konstantin Kolodiy <kkolodiy@luxoft.com>
2018-06-26 21:19:42 +0300

    Implement reading of the file

--- src/homeworks/student1/phones.py
+++ src/homeworks/student1/phones.py
@@ -1,11 +1,27 @@
 import random
+import string
+import os
 
 def create_phone():
-    choices = "0123456789"
-    return '9' + ''.join(random.choice(choices) for _ in range(9))
+    return '9' + ''.join(random.choice(string.digits) for _ in range(9))
 
 
 def generate_number_strings(n, name):
     with open(name, 'w') as f:
         for _ in range(n):
             f.write(create_phone() + '\n')
+
+
+def check_format(phone):
+    return phone.isdigit() is True and len(phone) == 10 and phone[0] == '9'
+
+
+def read_numbers(name):
+    try:
+        with open(name) as f:
+            for line in f:
+                phone = line.strip()
+                if check_format(phone):
+                    yield phone
+    except OSError:
+         pass
--- src/homeworks/student1/test_phones.py
+++ src/homeworks/student1/test_phones.py
@@ -1,5 +1,6 @@
 import os.path
 from .phones import create_phone, generate_number_strings
+from .phones import read_numbers
 
 def test_phone_len_is_10():
     assert 10 == len(create_phone())
@@ -25,4 +26,22 @@ def test_file_contains_5_lines():
     name = "phones_test.txt"
     generate_number_strings(5, name)
     assert 5 == len(open(name).readlines())
+    os.remove(name)
+
+
+def test_read_file_with_3_lines():
+    name = "phones_test.txt"
+    with open(name, 'w') as f: f.writelines(['9123456789\n', '9234567890\n', '9345678901\n'])
+    assert 3 == len(list(read_numbers(name)))
+    os.remove(name)
+
+
+def test_read_not_existing_file():
+    assert 0 == len(list(read_numbers("this_file_does_not_exist.txt")))
+
+
+def test_correct_format_of_the_line():
+    name = "phones_test.txt"
+    open(name, 'w').writelines(["9123456789\n", "1234567890\n", "234\n", "9abc123efg\n"])
+    assert 1 == len(list(read_numbers(name)))
     os.remove(name)
